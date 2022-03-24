db59c2308a73e17397969b23c0cc0703497ec75f
Ian Leiman <ian.leiman@gmail.com>
2022-03-24 20:00:30 +0200

    p062 added

---
+++ p062_euler.js
@@ -0,0 +1,111 @@
+/** https://projecteuler.net/problem=62
+ * solution in Javascript
+ * ian.leiman@gmail.com
+ * 
+ * The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104 (384^3) and 66430125 (405^3). 
+ * In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
+ * Find the smallest cube for which exactly five permutations of its digits are cube.
+ * 
+ * execute this on command line if you have node installed
+ * node .\p062_euler.js
+ * 
+ * Answer:  127035954683
+ */
+
+/**
+ * the first approach can be used to study cubes that have these permutations
+ */
+console.time("1");
+
+function distSig(x){
+    // calculates a digit distribution signature of a number
+    // returns number of digits and array with how many digits each the number contains
+    let xs = String(x);
+    let sig = [0,0,0,0,0,0,0,0,0,0,0];
+    siglen = xs.length; // set the length of the number in digits
+    for (let i=xs.length-1; i >= 0; i--){ // check all the digits 
+        digit = xs[i] ; // get the decimal digit
+        sig[digit]++; // increment the count of this digit
+    }
+    // let sigstr = sig.reverse().join("");
+    let sigstr = sig.join("");
+    return [siglen, sigstr]; //parseInt( sigstr );
+}
+
+// configuration settings for this algorithm, can tweak to use for further 
+// numerology studies
+// the below settings will find all 12 digit cubes that have 5 permutations
+let printLimit = 3; // print only 5 permutation sets
+let maxRoot = 9999; // biggest root that has 12 digits
+let minRoot = 4642; // smallest root that has 12 digits
+let maxDig = 13; // stop before 13 digits 
+let minDig =12;  // start from 12 digits
+// internal variables
+let root = 0;
+let cube = 0;
+let sig = []; 
+let sigbylen = [];
+
+// init arrays for each digit lengths
+for (i= minDig; i < maxDig; i++){
+    sigbylen[i] = Array();
+}
+
+for (root=minRoot; root < maxRoot; root++){
+    cube = root*root*root;
+    sig = distSig(cube);
+    sigbylen[sig[0]].push( [root, cube, parseInt( sig[1] ) ] );
+    // console.log(root, cube, sig);
+}
+
+for (i= minDig; i < maxDig; i++){
+    let sl = sigbylen[i].length;
+    console.log("***", i, sl);
+    let sarr = sigbylen[i];
+    let sigsorted = sarr.sort(function (a,b){return a[2] - b[2] });
+
+    let key = 0;
+    let count = 0;
+    let sa = [];
+    for(j=0; j < sl; j++){
+        let thisSig = sigsorted[j][2];
+        if (thisSig != key) {
+            if (count > printLimit){
+                console.log( key, count+1, sa);
+            }
+            key = thisSig;
+            count = 0;
+            sa = [];
+        } else {
+            if (count == 0) sa.push(sigsorted[j-1][1]); 
+            count++; 
+            sa.push(sigsorted[j][1]);  
+
+        }
+        //console.log( sigsorted[j] );
+
+    }
+}
+console.timeEnd("1");
+
+/**
+ * another approach that is simpler and more brute force, slightly slower
+ */
+console.time("2");
+
+let oCube = [];
+for(var i=0; true; i++){
+	var mCube = String(i*i*i);
+	var cubeNum = mCube.split('');
+	cubeNum.sort();
+	var p = cubeNum.join('')
+	if(oCube[p] == undefined){
+		oCube[p] = [];
+	}
+	oCube[p].push(mCube);
+	if(oCube[p].length>=5){
+		console.log(oCube[p]);
+		break;
+	}
+}
+console.timeEnd("2");
